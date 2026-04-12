04b49c8c504cee6356860add960d359d1e0202be
Joe Sutton <hungryed6@gmail.com>
2026-04-11 20:26:16 -0700

    Remove unused variable

--- frontend/packages/apps/bikepelago-app/vite.config.ts
+++ frontend/packages/apps/bikepelago-app/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env instead of just those starting with `VITE_`.
+export default defineConfig(() => {
   const apiUrl = process.env.VITE_PUBLIC_API_URL || process.env.VITE_API_URL || 'http://127.0.0.1:5054';
   
   return {
