41a66aa6d59e75e5a4f5e1e1a23a2531132d5902
Joe Sutton <hungryed6@gmail.com>
2026-04-11 20:03:03 -0700

    Docker compose touchups

--- frontend/packages/apps/bikepelago-app/vite.config.ts
+++ frontend/packages/apps/bikepelago-app/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env instead of just those starting with `VITE_`.
-  const env = loadEnv(mode, process.cwd(), '');
-  const apiUrl = env.VITE_PUBLIC_API_URL || env.VITE_API_URL || 'http://127.0.0.1:5054';
+  const apiUrl = process.env.VITE_PUBLIC_API_URL || process.env.VITE_API_URL || 'http://127.0.0.1:5054';
   
   return {
     plugins: [react()],
